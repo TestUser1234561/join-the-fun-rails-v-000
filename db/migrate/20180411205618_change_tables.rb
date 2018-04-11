dbd79412c7fb08cbc4672dbb94f66f0e05694207
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 20:51:08 +0000

    automatically backed up by learn

--- db/migrate/20180411205618_change_tables.rb
+++ db/migrate/20180411205618_change_tables.rb
@@ -1,6 +1,6 @@
 class ChangeTables < ActiveRecord::Migration
   def change
-    add_column :rides, :taxi_id, :belongs_to
-    add_column :rides, :passenger_id, :belongs_to
+    add_column :rides, :taxi, :belongs_to
+    add_column :rides, :passenger, :belongs_to
   end
 end
