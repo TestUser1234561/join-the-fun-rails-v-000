70777180fc9eeab00f200d24a1a0e1eac5f58b1f
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 20:51:17 +0000

    automatically backed up by learn

--- db/migrate/20180411205618_change_tables.rb
+++ db/migrate/20180411205618_change_tables.rb
@@ -1,6 +1,6 @@
 class ChangeTables < ActiveRecord::Migration
   def change
-    add_column :rides, :taxi, :belongs_to
-    add_column :rides, :passenger, :belongs_to
+    add_column :rides, :taxi, :belongs_to, index: true
+    add_column :rides, :passenger, :belongs_to, index: true
   end
 end
