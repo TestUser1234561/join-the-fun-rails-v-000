c948a920f2457bb2ec2b9dba4eac186b5c1bc7b4
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 20:34:22 +0000

    automatically backed up by learn

--- db/migrate/004_change_tables.rb
+++ db/migrate/004_change_tables.rb
@@ -1,6 +1,6 @@
 class ChangeTables < ActiveRecord::Migration
   def change
     add_foreign_key :taxis, :rides
-    add_foreign_key :taxis, :rides
+    add_foreign_key :passengers, :rides
   end
 end
